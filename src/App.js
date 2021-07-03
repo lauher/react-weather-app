93212ca089f902f06cef52001727ec0e1006e12a
lauher <lauraherbert90@hotmail.com>
2021-07-03 16:52:23 +0200

    Cleaned up unnecessary React files

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import "./App.css";
 import Weather from "./Weather";
 import TestAxios from "./TestAxios";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1> hello World</h1>
       <TestAxios />
       <Weather />
     </div>
   );
 }
-
-export default App;
