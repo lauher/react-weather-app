0b6ea5dfb43e6b7b7984555b879188c288eb0b97
lauher <lauraherbert90@hotmail.com>
2021-07-18 08:14:26 +0200

    Removed the TestAxios component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Weather from "./Weather";
-import TestAxios from "./TestAxios";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <TestAxios />
         <Weather />
       </div>
     </div>
